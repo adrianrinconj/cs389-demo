22b04e61765c9266f9ed885686157ac7b48e4773
Adrian Rincon <arincon-jimenez@carroll.edu>
2023-08-28 00:34:47 -0600

    Controller for responding to root context requests

---
+++ src/main/java/edu/carroll/cs389/HelloController.java
@@ -0,0 +1,24 @@
+//package main.java.edu.carroll.cs389;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class HelloController {
+//    @GetMapping("/")
+//    public String index() {
+//        return "Greetings from Spring Boot!";
+//    }
+//}
+package edu.carroll.cs389;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
