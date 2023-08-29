c2037c3d3db189a1b40cc69ac2161020daea35ec
Adrian Rincon <arincon-jimenez@carroll.edu>
2023-08-28 20:20:31 -0600

    Controller tester working with controller

---
+++ src/test/java/edu/carroll/cs389/HelloControllerTest.java
@@ -0,0 +1,37 @@
+package edu.carroll.cs389;
+
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void indexTest() throws Exception {
+        mockMvc.perform(get("/")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, Student!")));
+    }
+    @Test
+    public void indexWithParamTest() throws Exception {
+        final String name = "You";
+        mockMvc.perform(get("/").param("name", name)).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, " + name + "!")));
+    }
+}
