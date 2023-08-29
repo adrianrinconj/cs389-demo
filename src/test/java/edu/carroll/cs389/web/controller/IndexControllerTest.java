3128a37b628c243dbfd2d05fac10da131d432349
Adrian Rincon <arincon-jimenez@carroll.edu>
2023-08-28 21:20:04 -0600

    removed Controller and ControllerTest classes and now have IndexController and IndexControllerTest classes

--- src/test/java/edu/carroll/cs389/web/controller/IndexControllerTest.java
+++ src/test/java/edu/carroll/cs389/web/controller/IndexControllerTest.java
@@ -1,8 +1,8 @@
-package edu.carroll.cs389;
-
+package edu.carroll.cs389.web.controller;
+//package edu.carroll.cs389.web.controller;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -12,12 +12,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestParam;
 
-
-@WebMvcTest(HelloController.class)
-public class HelloControllerTest {
+@WebMvcTest(IndexController.class)
+public class IndexControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
@@ -25,13 +22,7 @@ public class HelloControllerTest {
     public void indexTest() throws Exception {
         mockMvc.perform(get("/")).andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello, Student!")));
-    }
-    @Test
-    public void indexWithParamTest() throws Exception {
-        final String name = "You";
-        mockMvc.perform(get("/").param("name", name)).andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello, " + name + "!")));
+                .andExpect(content().string(containsString("Welcome to the application!")))
+                .andExpect(content().string(containsString("login")));
     }
 }
