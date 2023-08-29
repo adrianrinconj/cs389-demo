c903c5d0ae1b70762253d9e8772597a56a85585d
Adrian Rincon <arincon-jimenez@carroll.edu>
2023-08-28 21:04:11 -0600

    new commit since git pushing was not working to my GH account

--- src/main/java/edu/carroll/cs389/HelloController.java
+++ src/main/java/edu/carroll/cs389/HelloController.java
@@ -10,15 +10,30 @@
 //        return "Greetings from Spring Boot!";
 //    }
 //}
+//package edu.carroll.cs389;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class HelloController {
+//    @GetMapping("/hello")
+//    public String index() {
+//        return "Hello from Spring Boot!";
+//    }
+//}
+
 package edu.carroll.cs389;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
 public class HelloController {
-    @GetMapping("/hello")
-    public String index() {
-        return "Hello from Spring Boot!";
+    @GetMapping("/")
+    public String index(@RequestParam(value="name", required=false, defaultValue="Student")String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
     }
 }
